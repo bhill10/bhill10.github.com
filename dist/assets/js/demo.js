d946826a2b1fa34efbb7628057f2fffced87b589
andysolomon <andrewsolomon.edu@gmail.com>
2014-10-26 21:09:31 -0400

    initial commit

---
+++ dist/assets/js/demo.js
@@ -0,0 +1,91 @@
+var MainNavToggle = {
+};
+
+var FitVids = {
+    init: function () {
+        $(document).ready(function(){
+        // Target your .container, .wrapper, .post, etc.
+            $(".video-gallery").fitVids();
+        });
+    }
+};
+
+// Allow anchors to gracefully scroll into view.  Will be removed shortly
+var ScrollIntoView = {
+};
+
+var DropkickSelect = {
+};
+
+
+var ClickHomepageImage = {
+};
+
+
+var Optimizations = {
+};
+
+// Maintain Homepage Grid with offset usage in javascript and jquery
+var MaintainHomepageGrid = {
+};
+
+var SlidrCarousel = {
+    init: function (){
+        var s = slidr.create('slidr-hero')
+        var vs = slidr.create('slidr-video')
+        s.add('h', ['one', 'two', 'three', 'one']);
+        s.auto();
+
+        vs.add('h', ['one', 'two', 'three', 'one']);
+        vs.start();
+    } // End init function
+};
+
+var Overlay = {
+    init: function() {
+
+        var triggerBttn = $( '.li-img' ),
+        overlay = $( '.overlay' ),
+        closeBttn = $( 'button.overlay-close' );
+
+        function toggleOverlay() {
+            if( !overlay.hasClass('open')) {
+                $('.overlay nav').html( '' );
+                overlay.removeClass('close');
+                overlay.addClass('open');
+                //overlayImage = $(this).find( 'img' )[0];
+                //console.log( $(this) );
+                //console.log( $(this).find('img')[0] );
+                //$(this).clone().appendTo( '.overlay nav' );
+                //$( '.overlay nav' ).html(overlayImage);
+            }else
+            if( overlay.hasClass( 'open' ) ) {
+                overlay.addClass('close');
+                overlay.removeClass('open');
+            }
+        }
+
+        $(triggerBttn).on('click', toggleOverlay);
+        $(closeBttn).on('click', toggleOverlay);
+
+    }
+}
+
+var Fluidbox = {
+    init: function() {
+
+        $(' .gallery a ').fluidbox();
+
+    }
+}
+
+SlidrCarousel.init();
+//Overlay.init();
+//Fluidbox.init();
+//FitVids.init();
+//MainNavToggle.init();
+//ScrollIntoView.init();
+//DropkickSelect.init();
+//ClickHomepageImage.init();
+//Optimizations.init();
+//MaintainHomepageGrid.init();
---
+++ src/data/how-does-it-work__content.md
@@ -0,0 +1,4 @@
+There are many creative ways to help you out of any situation. We pride
+ourselves on our reputation for working one-on-one with each customer to handle
+each individual situation; and it’s our goal to make each client feel like we
+achieved a WIN-WIN scenario.
